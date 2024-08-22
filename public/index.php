7ab6fe429fc04bb01d88a2bf47451ff9905247c2
exi66 <eternityexi@gmail.com>
2024-08-22 18:42:14 +0300

    исправлено: ошибка в локализации

--- public/index.php
+++ public/index.php
@@ -15,8 +15,8 @@ $_SESSION['__token'] = md5(uniqid(mt_rand(), true));
   <meta name="copyright" content="exi66" />
   <meta name="robots" content="index, follow" />
   <link rel="icon" href="/favicon.ico">
-  <link rel="stylesheet" href="./assets/main.css?v28" />
-  <script defer type="module" src="./assets/main.js?v28"></script>
+  <link rel="stylesheet" href="./assets/main.css?v29" />
+  <script defer type="module" src="./assets/main.js?v29"></script>
   <title>About me</title>
 </head>
 
